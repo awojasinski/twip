4536ce6b90dff449f102f81093595b735ced3616
awojasinski <a.wojasinski@gmail.com>
2021-05-10 19:49:52 +0200

    Add UART to project

--- hal/Core/Inc/stm32g4xx_it.h
+++ hal/Core/Inc/stm32g4xx_it.h
@@ -61,6 +61,7 @@ void EXTI1_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void I2C1_EV_IRQHandler(void);
 void USART1_IRQHandler(void);
+void USART2_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
