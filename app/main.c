00f3acfea00442c20517b0bf48ec8f47479b6b89
awojasinski <a.wojasinski@gmail.com>
2021-08-07 12:54:12 +0200

    main cleanup - align to invensense module

--- app/main.c
+++ app/main.c
@@ -10,16 +10,14 @@
 #include "usart.h"
 #include "gpio.h"
 
+//#include "ina219.h"
+//#include "mpu9250.h"
 #include "cli.h"
-#include "mpu9250.h"
 #include "logger.h"
 #include "encoder.h"
-#include "ina219.h"
+#include "invensense9250.h"
 
-volatile uint32_t pulses;
-
-static inline void fatfs_test(void);
-//static void scan_i2c(I2C_HandleTypeDef*, uint16_t, uint16_t);
+static inline void scan_i2c(I2C_HandleTypeDef *, uint16_t, uint16_t);
 
 void main(void) {
 
@@ -36,15 +34,15 @@ void main(void) {
   //MX_SPI1_Init();
   //MX_FATFS_Init();
   MX_I2C1_Init();
+  
   cli_init();
+  cli_mute(false);
   cli_info();
 
   HAL_Delay(100);
-
-  //scan_i2c(&hi2c1, 0, 0x100);
+  //scan_i2c(&hi2c1, 0, 0x0070);
 
   encoder_init(&htim3, &htim2, 90);
-  //ina219_init(&hi2c1);
 
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
   HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_4);
@@ -53,25 +51,53 @@ void main(void) {
   HAL_TIM_Base_Start_IT(&htim3);
 
   //fatfs_test();
-  //mpu9250_init();
-  //fatfs_test();
+  mpu9250_init();
+  //run_self_test();
+
   //HAL_SPI_MspDeInit(&hspi1);
   //logger_init();
-  cli_mute(false);
-
-  while (1) {
+  long accel[3], gyro[3], euler[3], lquat[4], heading;
+  float fquat[4];
+  while (1)
+  {
     HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-    pulses = htim3.Instance->CNT;
+    //pulses = htim3.Instance->CNT;
+    inv_get_accel(accel);
+    inv_get_gyro(gyro);
+    cli_printf("gyro: %7.4f %7.4f %7.4f",
+               inv_q16_to_float(gyro[0]),
+               inv_q16_to_float(gyro[1]),
+               inv_q16_to_float(gyro[2]));
+    cli_printf("accel: %7.4f %7.4f %7.4f",
+               inv_q16_to_float(accel[0]),
+               inv_q16_to_float(accel[1]),
+               inv_q16_to_float(accel[2]));
+    inv_get_6axis_quaternion(lquat);
+      cli_printf("lquat: %7.4f %7.4f %7.4f %7.4f",
+                 inv_q30_to_float(lquat[0]),
+                 inv_q30_to_float(lquat[1]),
+                 inv_q30_to_float(lquat[2]),
+                 inv_q30_to_float(lquat[3]));
+    inv_get_sensor_type_euler(euler, NULL, NULL);
+      cli_printf("euler: %7.4f %7.4f %7.4f",
+                 inv_q16_to_float(euler[0]),
+                 inv_q16_to_float(euler[1]),
+                 inv_q16_to_float(euler[2]));
+    inv_get_sensor_type_heading(&heading, NULL, NULL);
+    cli_printf("heading: %7.4f", inv_q16_to_float(heading));
+    inv_get_quaternion_float(fquat);
+      cli_printf("fquat: %7.4f %7.4f %7.4f %7.4f",
+                 fquat[0],
+                 fquat[1],
+                 fquat[2],
+                 fquat[3]);
     //cli_printf("Pulses: %4u Angle: %6.2f Velo: %6.2f", pulses, encoder_get_angle_deg((encoder_t*)&encoder_left), encoder_left.ang_velo);
-    cli_printf("PWM:%d RPM:%lu", __HAL_TIM_GET_COMPARE(&htim1, TIM_CHANNEL_1)/10, encoder_left.velo); //encoder_get_angle_deg(&encoder_left));
-    //cli_printf("%")
-    cli_delay(10);
-    cli_clear_line(1);
+    cli_delay(100);
+    cli_clear_line(6);
   }
 }
 
-/*
-static void scan_i2c(I2C_HandleTypeDef *i2c, uint16_t start_addr, uint16_t stop_addr) {
+static inline void scan_i2c(I2C_HandleTypeDef *i2c, uint16_t start_addr, uint16_t stop_addr) {
   cli_printf("Scan I2C addresses");
   for (uint16_t addr = start_addr; addr <= stop_addr; addr++) {
     if( HAL_I2C_IsDeviceReady(i2c, addr << 1, 5, 200) == HAL_OK) {
@@ -85,96 +111,4 @@ static void scan_i2c(I2C_HandleTypeDef *i2c, uint16_t start_addr, uint16_t stop_
   }
   cli_printf_inline("\n\r");
   HAL_Delay(10);
-}
-*/
-
-static inline void fatfs_test(){
-  FATFS FS;
-  FIL f;
-  FRESULT res;
-
-  res = f_mount(&FS, "", 1); //1=mount now
-  if (res != FR_OK)
-  {
-    cli_printf("f_mount error (%i)\r\n", res);
-    while (1)
-      ;
-  }
-
-  //Let's get some statistics from the SD card
-  DWORD free_clusters, free_sectors, total_sectors;
-
-  FATFS *getFreeFs;
-
-  res = f_getfree("", &free_clusters, &getFreeFs);
-  if (res != FR_OK)
-  {
-    cli_printf("f_getfree error (%i)\r\n", res);
-    while (1)
-      ;
-  }
-
-  //Formula comes from ChaN's documentation
-  total_sectors = (getFreeFs->n_fatent - 2) * getFreeFs->csize;
-  free_sectors = free_clusters * getFreeFs->csize;
-
-  cli_printf("SD card stats:\r\n%10lu KiB total drive space.\r\n%10lu KiB available.\r\n", total_sectors / 2, free_sectors / 2);
-
-  //Now let's try to open fe "test.txt"
-  res = f_open(&f, "test.txt", FA_READ);
-  if (res != FR_OK)
-  {
-    cli_printf("f_open error (%i)\r\n");
-    while (1)
-      ;
-  }
-  cli_printf("I was able to open 'test.txt' for reading!\r\n");
-
-  //Read 30 bytes from "test.txt" on the SD card
-  BYTE readBuf[30];
-
-  //We can either use f_read OR f_gets to get data out of fes
-  //f_gets is a wrapper on f_read that does some string formatting for us
-  TCHAR *rres = f_gets((TCHAR *)readBuf, 30, &f);
-  if (rres != 0)
-  {
-    cli_printf("Read string from 'test.txt' contents: %s\r\n", readBuf);
-  }
-  else
-  {
-    cli_printf("f_gets error (%i)\r\n", res);
-  }
-
-  //Be a tidy kiwi - don't forget to close your fe!
-  f_close(&f);
-
-  //Now let's try and write a fe "write.txt"
-  res = f_open(&f, "write.txt", FA_WRITE | FA_OPEN_ALWAYS | FA_CREATE_ALWAYS);
-  if (res == FR_OK)
-  {
-    cli_printf("I was able to open 'write.txt' for writing\r\n");
-  }
-  else
-  {
-    cli_printf("f_open error (%i)\r\n", res);
-  }
-
-  //Copy in a string
-  strncpy((char *)readBuf, "a new file is made!", 19);
-  UINT bytesWrote;
-  res = f_write(&f, readBuf, 19, &bytesWrote);
-  if (res == FR_OK)
-  {
-    cli_printf("Wrote %i bytes to 'write.txt'!\r\n", bytesWrote);
-  }
-  else
-  {
-    cli_printf("f_write error (%i)\r\n");
-  }
-
-  //Be a tidy kiwi - don't forget to close your fe!
-  f_close(&f);
-
-  //We're done, so de-mount the drive
-  f_mount(NULL, "", 1);
 }
--- libs/invensense9250/invensense9250.c
+++ libs/invensense9250/invensense9250.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include "main.h"
+#include <math.h>
 
 #include "cli.h"
 #include "invensense9250.h"
@@ -7,14 +8,9 @@
 unsigned char accel_fsr, new_temp = 0;
 unsigned short gyro_rate, gyro_fsr;
 unsigned long sensor_timestamp;
-struct int_param_s int_param;
+static unsigned long timestamp;
 
-static struct hal_s hal = {0};
-
-/* USB RX binary semaphore. Actually, it's just a flag. Not included in struct
- * because it's declared extern elsewhere.
- */
-volatile unsigned char rx_new;
+static volatile struct hal_s hal = {0};
 
 unsigned char *mpl_key = (unsigned char *)"eMPL 5.1";
 
@@ -58,17 +54,14 @@ static struct platform_data_s compass_pdata = {
 
 #ifdef COMPASS_ENABLED
 unsigned short compass_fsr;
+unsigned char new_compass = 0;
+unsigned long compass_timestamp;
 #endif
 
 void mpu9250_init(void)
 {
-    inv_error_t result = inv_init_mpl();
-    if (result)
-    {
-        log_e("Could not initialize MPL.");
-    }
 
-    result = mpu_init(NULL);
+    inv_error_t result = mpu_init(NULL);
     if (result)
     {
         log_e("Could not initialize gyro.");
@@ -79,11 +72,19 @@ void mpu9250_init(void)
     {
         log_i("MPU9250 initialized");
 
+        result = inv_init_mpl();
+        if (result)
+        {
+            log_e("Could not initialize MPL.");
+        }
+
         inv_enable_quaternion();
         inv_enable_9x_sensor_fusion();
 
         inv_enable_fast_nomot();
         inv_enable_gyro_tc();
+
+        inv_enable_in_use_auto_calibration();
 
 #ifdef COMPASS_ENABLED
         /* Compass calibration algorithms. */
@@ -112,13 +113,18 @@ void mpu9250_init(void)
 #endif
         /* Push both gyro and accel data into the FIFO. */
         mpu_configure_fifo(INV_XYZ_GYRO | INV_XYZ_ACCEL);
-        mpu_set_sample_rate(1000);
-
+        mpu_set_sample_rate(5 * DEFAULT_MPU_HZ);
+#ifdef COMPASS_ENABLED
+        /* The compass sampling rate can be less than the gyro/accel sampling rate.
+       * Use this function for proper power management.
+       */
+        mpu_set_compass_sample_rate(1000 / COMPASS_READ_MS);
+#endif
         /* Read back configuration in case it was set improperly. */
         mpu_get_sample_rate(&gyro_rate);
         mpu_get_gyro_fsr(&gyro_fsr);
-        log_i("Sampling rate: %d", gyro_rate);
         mpu_get_accel_fsr(&accel_fsr);
+        mpu_set_lpf(20);
 #ifdef COMPASS_ENABLED
         mpu_get_compass_fsr(&compass_fsr);
 #endif
@@ -126,6 +132,7 @@ void mpu9250_init(void)
         /* Sample rate expected in microseconds. */
         inv_set_gyro_sample_rate(1000000L / gyro_rate);
         inv_set_accel_sample_rate(1000000L / gyro_rate);
+        inv_set_quat_sample_rate(1000000L / gyro_rate);
 #ifdef COMPASS_ENABLED
         /* The compass rate is independent of the gyro and accel rates. As long as
      * inv_set_compass_sample_rate is called with the correct value, the 9-axis
@@ -160,39 +167,43 @@ void mpu9250_init(void)
         hal.next_temp_ms = 0;
 
         /* Compass reads are handled by scheduler. */
-        get_tick(&sensor_timestamp);
-
+        get_tick(&timestamp);
         mpu9250_backend_init();
 
-        hal.dmp_features = /**DMP_FEATURE_6X_LP_QUAT |*/ DMP_FEATURE_SEND_RAW_ACCEL | DMP_FEATURE_SEND_CAL_GYRO |
+        hal.dmp_features = DMP_FEATURE_6X_LP_QUAT |
+                           DMP_FEATURE_SEND_RAW_ACCEL |
+                           DMP_FEATURE_SEND_CAL_GYRO |
                            DMP_FEATURE_GYRO_CAL;
-        
+
         mpu9250_backend_config(&hal.dmp_features);
 
         HAL_NVIC_SetPriority(EXTI4_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ(EXTI4_IRQn);
-
     }
 }
 
 void mpu9250_backend_init()
 {
     inv_error_t result = dmp_load_motion_driver_firmware();
-    if (result)
+
+    if (result != 0)
     {
         log_e("Could not load DMP firmware.");
-    } else {
+    }
+    else
+    {
         hal.dmp_on = 1;
         MPL_LOGI("DMP enabled.");
     }
 }
 
-void mpu9250_backend_config(unsigned short * features)
+void mpu9250_backend_config(unsigned short *features)
 {
     dmp_set_orientation(
         inv_orientation_matrix_to_scalar(gyro_pdata.orientation));
     dmp_enable_feature(hal.dmp_features);
-    dmp_set_fifo_rate(40);
+    dmp_set_fifo_rate(DEFAULT_MPU_HZ);
+    //dmp_set_interrupt_mode(DMP_INT_CONTINUOUS);
     mpu_set_dmp_state(1);
     hal.dmp_on = 1;
 }
@@ -200,7 +211,26 @@ void mpu9250_backend_config(unsigned short * features)
 static inline void gyro_data_ready_cb(void)
 {
     hal.new_gyro = 1;
-    new_temp = 1;
+
+    unsigned long timestamp;
+
+    get_tick(&timestamp);
+#ifdef COMPASS_ENABLED
+    if ((timestamp > hal.next_compass_ms) && !hal.lp_accel_mode &&
+        hal.new_gyro && (hal.sensors & COMPASS_ON))
+    {
+        hal.next_compass_ms = timestamp + COMPASS_READ_MS;
+        new_compass = 1;
+    }
+#endif
+    /* Temperature data doesn't need to be read with every gyro sample.
+	           * Let's make them timer-based like the compass reads.
+	           */
+    if (timestamp > hal.next_temp_ms)
+    {
+        hal.next_temp_ms = timestamp + TEMP_READ_MS;
+        new_temp = 1;
+    }
 }
 
 void run_self_test(void)
@@ -280,117 +310,10 @@ void run_self_test(void)
     }
 }
 
-static inline void read_from_mpl(void)
-{
-    long msg, data[9];
-    int8_t accuracy;
-    unsigned long timestamp;
-    float float_data[3] = {0};
-
-    if (inv_get_sensor_type_quat(data, &accuracy, (inv_time_t *)&timestamp))
-    {
-        /* Sends a quaternion packet to the PC. Since this is used by the Python
-        * test app to visually represent a 3D quaternion, it's sent each time
-        * the MPL has new data.
-        */
-        eMPL_send_quat(data);
-
-        /* Specific data packets can be sent or suppressed using USB commands. */
-        if (hal.report & PRINT_QUAT)
-            eMPL_send_data(PACKET_DATA_QUAT, data);
-    }
-
-    if (hal.report & PRINT_ACCEL)
-    {
-        if (inv_get_sensor_type_accel(data, &accuracy,
-                                      (inv_time_t *)&timestamp))
-            eMPL_send_data(PACKET_DATA_ACCEL, data);
-    }
-    if (hal.report & PRINT_GYRO)
-    {
-        if (inv_get_sensor_type_gyro(data, &accuracy,
-                                     (inv_time_t *)&timestamp))
-            eMPL_send_data(PACKET_DATA_GYRO, data);
-    }
-#ifdef COMPASS_ENABLED
-    if (hal.report & PRINT_COMPASS)
-    {
-        if (inv_get_sensor_type_compass(data, &accuracy,
-                                        (inv_time_t *)&timestamp))
-            eMPL_send_data(PACKET_DATA_COMPASS, data);
-    }
-#endif
-    if (hal.report & PRINT_EULER)
-    {
-        if (inv_get_sensor_type_euler(data, &accuracy,
-                                      (inv_time_t *)&timestamp))
-            eMPL_send_data(PACKET_DATA_EULER, data);
-    }
-    if (hal.report & PRINT_ROT_MAT)
-    {
-        if (inv_get_sensor_type_rot_mat(data, &accuracy,
-                                        (inv_time_t *)&timestamp))
-            eMPL_send_data(PACKET_DATA_ROT, data);
-    }
-    if (hal.report & PRINT_HEADING)
-    {
-        if (inv_get_sensor_type_heading(data, &accuracy,
-                                        (inv_time_t *)&timestamp))
-            eMPL_send_data(PACKET_DATA_HEADING, data);
-    }
-    if (hal.report & PRINT_LINEAR_ACCEL)
-    {
-        if (inv_get_sensor_type_linear_acceleration(float_data, &accuracy, (inv_time_t *)&timestamp))
-        {
-            MPL_LOGI("Linear Accel: %7.5f %7.5f %7.5f\r\n",
-                     float_data[0], float_data[1], float_data[2]);
-        }
-    }
-    if (hal.report & PRINT_GRAVITY_VECTOR)
-    {
-        if (inv_get_sensor_type_gravity(float_data, &accuracy,
-                                        (inv_time_t *)&timestamp))
-            MPL_LOGI("Gravity Vector: %7.5f %7.5f %7.5f\r\n",
-                     float_data[0], float_data[1], float_data[2]);
-    }
-    if (hal.report & PRINT_PEDO)
-    {
-        unsigned long timestamp;
-        get_tick(&timestamp);
-        if (timestamp > hal.next_pedo_ms)
-        {
-            hal.next_pedo_ms = timestamp + PEDO_READ_MS;
-            unsigned long step_count, walk_time;
-            dmp_get_pedometer_step_count(&step_count);
-            dmp_get_pedometer_walk_time(&walk_time);
-            MPL_LOGI("Walked %ld steps over %ld milliseconds..\n", step_count,
-                     walk_time);
-        }
-    }
-
-    /* Whenever the MPL detects a change in motion state, the application can
-     * be notified. For this example, we use an LED to represent the current
-     * motion state.
-     */
-    msg = inv_get_message_level_0(INV_MSG_MOTION_EVENT |
-                                  INV_MSG_NO_MOTION_EVENT);
-    if (msg)
-    {
-        if (msg & INV_MSG_MOTION_EVENT)
-        {
-            MPL_LOGI("Motion!\n");
-        }
-        else if (msg & INV_MSG_NO_MOTION_EVENT)
-        {
-            MPL_LOGI("No motion!\n");
-        }
-    }
-}
-
 void EXTI4_IRQHandler(void)
 {
     gyro_data_ready_cb();
-    while (hal.new_gyro && hal.dmp_on)
+    if (hal.new_gyro && hal.dmp_on)
     {
         short gyro[3], accel_short[3], sensors;
         unsigned char more;
@@ -398,59 +321,51 @@ void EXTI4_IRQHandler(void)
 
         dmp_read_fifo(gyro, accel_short, quat, &sensor_timestamp, &sensors, &more);
         if (!more)
+        {
             hal.new_gyro = 0;
+        }
         if (sensors & INV_XYZ_GYRO)
         {
-            /* Push the new data to the MPL. */
             inv_build_gyro(gyro, sensor_timestamp);
             if (new_temp)
             {
                 new_temp = 0;
-                /* Temperature only used for gyro temp comp. */
                 mpu_get_temperature(&temperature, &sensor_timestamp);
                 inv_build_temp(temperature, sensor_timestamp);
             }
+            inv_execute_on_data();
         }
         if (sensors & INV_XYZ_ACCEL)
         {
             accel[0] = (long)accel_short[0];
             accel[1] = (long)accel_short[1];
             accel[2] = (long)accel_short[2];
-            inv_build_accel(accel, 0, sensor_timestamp);
+            inv_build_accel(accel, 3, sensor_timestamp);
+            inv_execute_on_data();
         }
         if (sensors & INV_WXYZ_QUAT)
         {
-            inv_build_quat(quat, 0, sensor_timestamp);
+            inv_build_quat(quat, 9, sensor_timestamp);
         }
     }
-    
+
 #ifdef COMPASS_ENABLED
-    short compass_short[3];
-    long compass[3];
-    /* For any MPU device with an AKM on the auxiliary I2C bus, the raw
-         * magnetometer registers are copied to special gyro registers.
-         */
-    if (!mpu_get_compass_reg(compass_short, &sensor_timestamp))
+    if (new_compass)
     {
-        compass[0] = (long)compass_short[0];
-        compass[1] = (long)compass_short[1];
-        compass[2] = (long)compass_short[2];
-        /* NOTE: If using a third-party compass calibration library,
-             * pass in the compass data in uT * 2^16 and set the second
-             * parameter to INV_CALIBRATED | acc, where acc is the
-             * accuracy from 0 to 3.
-             */
-        inv_build_compass(compass, 0, sensor_timestamp);
+        short compass_short[3];
+        long compass[3];
+        new_compass = 0;
+        if (!mpu_get_compass_reg(compass_short, &sensor_timestamp))
+        {
+            compass[0] = (long)compass_short[0];
+            compass[1] = (long)compass_short[1];
+            compass[2] = (long)compass_short[2];
+            inv_build_compass(compass, 0, sensor_timestamp);
+            inv_execute_on_data();
+        }
     }
 #endif
+    //
     inv_execute_on_data();
-    /* This function reads bias-compensated sensor data and sensor
-         * fusion outputs from the MPL. The outputs are formatted as seen
-         * in eMPL_outputs.c. This function only needs to be called at the
-         * rate requested by the host.
-         */
-
-    hal.report = PRINT_ACCEL | PRINT_GYRO | PRINT_EULER | PRINT_QUAT;
-    //read_from_mpl();
     HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
 }
